816f354b4d24cda3eb29c991d8b4eea2e6bcbedb
SangeethKumarPG <pgsangeethkumar@gmail.com>
2025-10-27 14:01:33 +0530

    adding notes and code for mysql and postgres sql

--- todo_app/database.py
+++ todo_app/database.py
@@ -2,10 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_ALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
+# SQL_ALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
+# POSTGRES_URL = (
+#     "postgresql://postgres:admin%40123@localhost:5432/TodoApplicationDatabase"
+# )
 
-engine = create_engine(
-    SQL_ALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+MYSQL_URL = "mysql+pymysql://root:admin1234@127.0.0.1:3306/todoapplicationdatabase"
+# engine = create_engine(
+#     SQL_ALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+
+# engine = create_engine(POSTGRES_URL)
+engine = create_engine(MYSQL_URL)
 LocalSession = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 Base = declarative_base()
